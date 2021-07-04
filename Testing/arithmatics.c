a8d3476bad9d8aeea534df1d41b8a5ce77a92fce
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-04 08:06:05 -0700

    test

---
+++ Testing/arithmatics.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=5,c=9,d=6;
+
+    printf("\n\t a+c=");
+    printf("%d ",a+c);
+    printf("\n\t b+d=%d",b+d);
+    a=a+b+c;
+    printf("\n\t a+b+c=%d",a);
+
+    printf("\n\n\nSuccessfully Executed...\n");
+    return 0;
+}
+
