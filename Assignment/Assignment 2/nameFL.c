cecb253f14a56b750d83a08a3d48f6ae34bc9e24
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-04 07:32:48 -0700

    Name First  Letter

---
+++ Assignment/Assignment 2/nameFL.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n My Name First Letter");
+    printf("\n\n\n\t*********\n\t*\t *\n\t*\t  *\n\t*\t *");
+    printf("\n\t*********\n\t*\n\t*\n\t*\n\t* \n\n");
+    return 0;
+}
