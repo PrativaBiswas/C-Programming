0b715e690e895c2732ada805f42a504d36867e49
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-04 07:56:57 -0700

    delete

--- PROJECT/diya.c
+++
@@ -1,9 +0,0 @@
-//diya
-#include<stdio.h>
-int main()
-{
-    printf("\n\n\t\tHAPPY DIWALI\n\t\t------------");
-    printf("\n\n\n\t\t     *   \n\t\t    ***  \n\t\t   ***** \n\t\t    ***  \n\t\t     *   ");
-    printf("\n\t     *****************\n\t     *****************\n\t      ***************\n\t       *************\n\t         *********\n\n\n");
-    return 0;
-}
