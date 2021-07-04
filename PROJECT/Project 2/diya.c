b0e7a0c4d05638d790662693e2a94bc512aeaa98
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-04 08:03:21 -0700

    Project 2

---
+++ PROJECT/Project 2/diya.c
@@ -0,0 +1,9 @@
+//diya
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\t\tHAPPY DIWALI\n\t\t------------");
+    printf("\n\n\n\t\t     *   \n\t\t    ***  \n\t\t   ***** \n\t\t    ***  \n\t\t     *   ");
+    printf("\n\t     *****************\n\t     *****************\n\t      ***************\n\t       *************\n\t         *********\n\n\n");
+    return 0;
+}
