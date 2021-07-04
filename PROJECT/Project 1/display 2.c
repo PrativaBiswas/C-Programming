010b5607ffd8a4177e16b2c8f11f9ad2a9dd9bd2
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-04 08:00:22 -0700

    Project 1

---
+++ PROJECT/Project 1/display 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\t\t\tPRATIVA BISWAS :)\n");
+    printf("\n\t Details:\n\t\ ------------------------------------------------");
+    printf("\n\t |\tCollege   :  \tRITE\t\t\t|\n\t |\tAddress   :  \tJaydev Vihar\t\t|\n \t |\tPhone no  :  \t4567890987\t\t|\n");
+    printf("\t ------------------------------------------------\n\n\n ");
+    return 0;
+
+}
