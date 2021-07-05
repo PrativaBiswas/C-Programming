b5c38c295daec32653fdfba1e742c09a2901862b
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-05 05:53:56 -0700

    Volume of Cube

--- Assignment/Assignment 6/VolumeOfcube.c
+++ Assignment/Assignment 6/VolumeOfcube.c
@@ -2,14 +2,14 @@
 #include<stdio.h>
 int main()
 {
-    float side, area;
+    float side, volume;
 
     printf("\n\n   VOLUME OF CUBE\n   --------------");
     printf("\n\n     Enter the value of side of a cube : ");
     scanf("%f",&side);
 
-    area=side*side*side ;
-    printf("\n     Area of the cube = %f",area);
+    volume=side*side*side ;
+    printf("\n     Volume of the cube = %f",volume);
 
     printf("\n\n\n Successfully Executed....\n\n\n");
     return 0;
