9f08dcb5b1aa7b696983869d3044b7d18d318c22
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-17 04:37:48 -0700

    Assignment 9

---
+++ Assignment/Assignment 9/ASCII value/ASCIIvalue.c
@@ -0,0 +1,18 @@
+//ASCII value
+#include<stdio.h>
+int main()
+{
+    char name[10];
+
+    printf("\n\n\    ASCII VALUE OF YOUR FIRST AND SECOND LETTER OF MY NAME :\n    -------------------------------------------------------- ");
+    printf("\n\n\t\t Enter your name : ");
+    scanf("%s",&name);
+
+
+    printf("\n\n\t ASCII value : %d",name[0]);
+
+
+    printf("\n\n Successfully Executed....\n\n\n");
+    return 0;
+
+}
