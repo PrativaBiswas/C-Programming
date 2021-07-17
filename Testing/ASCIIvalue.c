615fe9ef8ad0623eb37f726f3b87dcff028ff729
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-17 04:27:33 -0700

    add

--- Testing/ASCIIvalue.c
+++ Testing/ASCIIvalue.c
@@ -6,11 +6,11 @@ int main()
 
     printf("\n\n\    ASCII VALUE OF YOUR FIRST AND SECOND LETTER OF MY NAME :\n    -------------------------------------------------------- ");
     printf("\n\n\t\t Enter your name : ");
-    scanf("%c%c",&name[0],&name[1]);
+    scanf("%s",&name);
 
 
     printf("\n\n\t ASCII value : %d",name[0]);
-    printf("\n\n\t ASCII value : %d",name[1]);
+
 
     printf("\n\n Successfully Executed....\n\n\n");
     return 0;
