d51255612050d9f3d77cebec8e2f6a15f9931ad2
Prativa_Biswas <biswasprativa6@gmail.com>
2021-07-24 08:41:54 -0700

    switch case

---
+++ Assignment/Assignment 10/EVEN_OR_ODD/even_or_odd.c
@@ -0,0 +1,27 @@
+//check the number even or odd by using switch case
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("\n\n      -------- NUMBER IS EVEN OR ODD BY SWITCH CASE--------");
+    printf("\n\n\t\t Enter a number for check : ");
+    scanf("%d",&num);
+
+    switch( num%2==0)
+    {
+     case 1:
+         printf("\n\n\t\t  The number %d is even",num);
+         break;
+     case 0:
+        printf("\n\n\t\t  The number %d id odd",num);
+        break;
+     default:
+        printf("\n\n\t WRONG INPUT ");
+
+    }
+
+    printf("\n\n\n   Successfully Executed....\n\n");
+    return 0;
+
+}
